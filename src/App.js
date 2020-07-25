b41f7bfb5dab1424fc9960bae27e4377b6264904
Görkem <gumutzkn@gmail.com>
2020-07-25 13:43:30 +0300

    added new components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import Homepage from "./pages/homepage/homepage.component";
-
 import "./App.css";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
